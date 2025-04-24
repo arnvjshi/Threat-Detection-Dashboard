7a0ee11348f2249c9e022801d8654d886ec19535
Arnav Joshi <arnvjshi@gmail.com>
2025-04-24 16:30:33 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'ThreatShield',
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics/>
       <body>{children}</body>
     </html>
   )
